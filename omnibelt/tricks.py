11d81f85ddbd2d9c88df8d7ff0da9b11326e9270
felixludos <felix.leeb@hotmail.com>
2022-04-17 15:20:38 +0200

    class descriptor deleter

--- omnibelt/tricks.py
+++ omnibelt/tricks.py
@@ -10,11 +10,23 @@ class ClassDescriptable(type):
 			super().__setattr__(key, val)
 
 
+	def __delattr__(self, item):
+		existing = inspect.getattr_static(self, item, None)
+		if isinstance(existing, classdescriptor):
+			existing.__delete__(self)
+		else:
+			super().__delattr__(item)
+
+
 
 class classdescriptor:
 	def __set__(self, obj, value):
 		raise NotImplementedError
-	
+
+
+	def __delete__(self, obj):
+		raise NotImplementedError
+
 
 
 class MRONamespace(dict):
@@ -137,6 +149,48 @@ class innerchild:
 			raise self.MissingParent(owner, cls_name)
 		child = type(cls_name, (self.cls, parent), {})
 		setattr(owner, name, child)
+
+
+
+
+
+# class Property(object):
+#     "Emulate PyProperty_Type() in Objects/descrobject.c"
+#
+#     def __init__(self, fget=None, fset=None, fdel=None, doc=None):
+#         self.fget = fget
+#         self.fset = fset
+#         self.fdel = fdel
+#         if doc is None and fget is not None:
+#             doc = fget.__doc__
+#         self.__doc__ = doc
+#
+#     def __get__(self, obj, objtype=None):
+#         if obj is None:
+#             return self
+#         if self.fget is None:
+#             raise AttributeError("unreadable attribute")
+#         return self.fget(obj)
+#
+#     def __set__(self, obj, value):
+#         if self.fset is None:
+#             raise AttributeError("can't set attribute")
+#         self.fset(obj, value)
+#
+#     def __delete__(self, obj):
+#         if self.fdel is None:
+#             raise AttributeError("can't delete attribute")
+#         self.fdel(obj)
+#
+#     def getter(self, fget):
+#         return type(self)(fget, self.fset, self.fdel, self.__doc__)
+#
+#     def setter(self, fset):
+#         return type(self)(self.fget, fset, self.fdel, self.__doc__)
+#
+#     def deleter(self, fdel):
+#         return type(self)(self.fget, self.fset, fdel, self.__doc__)
+
 
 
 
