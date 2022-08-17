fca6fbddedbf22d44c3bc3219a59bbc4909f2bcf
felixludos <felixludos.info@gmail.com>
2022-08-17 03:44:14 +0200

    minor update

--- omnibelt/__init__.py
+++ omnibelt/__init__.py
@@ -13,7 +13,7 @@ from .containers import deep_get, Simple_Child, Proper_Child, AttrDict, AttrOrdD
 from .registries import Registry, Entry_Registry, Named_Registry, Class_Registry
 from .logic import sort_by, resolve_order, toposort
 from .hashing import Hashable, md5
-from .utils import sign
+from .utils import sign, split_dict
 from .ordered_set import OrderedSet
 # from .exporting import export, load_export, Exportable
 from .exporting import export, load_export, Exporter
--- omnibelt/utils.py
+++ omnibelt/utils.py
@@ -1,6 +1,8 @@
 
 # from omnibelt import safe_self_execute
 
+from collections import OrderedDict
+
 def sign(x):
 	return 0 if x == 0 else (1 if x > 0 else -1)
 
@@ -20,6 +22,15 @@ def safe_self_execute(obj, fn, default='<<short circuit>>',
 	return out
 
 
+
+def split_dict(items, keys):
+	good, bad = OrderedDict(), OrderedDict()
+	for k in keys:
+		if k in keys:
+			good[k] = items[k]
+		else:
+			bad[k] = items[k]
+	return good, bad
 
 
 
