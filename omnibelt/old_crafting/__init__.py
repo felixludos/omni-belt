37da5491a02353583f57ed4796c2cca670a20bba
felixludos <felix.leeb@hotmail.com>
2023-03-27 18:12:33 +0200

    minor cleanup

--- omnibelt/old_crafting/__init__.py
+++
@@ -1,6 +0,0 @@
-from .abstract import AbstractCrafty, AbstractCraft, AbstractCrafts, AbstractRawCraft, \
-	AbstractCraftOperator, AbstractCraftsOperator
-from .crafty import BasicCrafty, InitializationCrafty
-from .crafts import ProcessedCrafts, ItemCrafts, InheritableCrafts, SeamlessCrafts, SeamlessInheritableCrafts
-from .craft import BasicRawCraft, SignatureRawCraft, RawCraftItem, BasicCraft, SignatureCraft, AwareCraft, \
-	WrappedCraft, PropertyCraft
