e9ec3a8417377a7f0a25db25231b5fa9c812ecf3
felixludos <felixludos.info@gmail.com>
2023-11-25 17:52:16 +0100

    fixed power formatting

--- omnibelt/utils.py
+++ omnibelt/utils.py
@@ -1,7 +1,8 @@
 
 # from omnibelt import safe_self_execute
-
-from typing import Iterator, Hashable
+import re
+import ast
+from typing import Iterator, Hashable, Any
 import sys
 from collections import OrderedDict
 from string import Formatter
@@ -84,103 +85,198 @@ def expression_format(s, **vars):
 
 
 class PowerFormatter(Formatter):
-	# TODO: partial formatting - only format fields that are specified, and leave others as is
+	def parse(self, s):
+		nodes = self.parse_bracket_tree(s)
+
+		idx = 0
+		for start, end, children in nodes:
+			if children is None:
+				if end > idx:
+					literal = s[idx:end]
+					yield literal, None, '', None
+					idx = end + 1
+			else:
+				content, spec, conv = self.parse_field(s[start + 1:end])
+				yield s[idx:start], content, spec, conv
+				idx = end + 1
+
+		if idx < len(s):
+			yield s[idx:], None, '', None
+
+	@staticmethod
+	def parse_bracket_tree(s):
+		'''
+		Parses a string with bracket escapes into a tree of bracketed fields
+
+		Output: [(start, end, children), ...]
+		where start and end are the indices of the brackets, and children is a list of children nodes
+		if children is None, then it's an escaped bracket
+		'''
+		stack = []
+		extra_closes = []
+		pairs = []
+		for i, c in enumerate(s):
+			if c == '{':
+				stack.append(i)
+			elif c == '}':
+				if len(stack):
+					start = stack.pop()
+					pairs.append((start, i))
+				else:
+					extra_closes.append(i)
+
+		# remove unpaired double bracket escapes
+		assert all(i + 1 in stack or i - 1 in stack for i in
+				   stack), f'Unbalanced opening brackets in {s!r} (remember to use double brackets to escape)'
+		assert all(i + 1 in extra_closes or i - 1 in extra_closes for i in
+				   extra_closes), f'Unbalanced closing brackets in {s!r} (remember to use double brackets to escape)'
+
+		escaped_nodes = ([(i, i + 1, None) for i in stack if i + 1 in stack]  # escaped opening brackets
+						 + [(i, i + 1, None) for i in extra_closes if
+							i + 1 in extra_closes])  # escaped closing brackets
+		escaped_pairs = [(start, end) for start, end in pairs if (start - 1, end + 1) in pairs]
+		for start, end in escaped_pairs:
+			pairs.remove((start - 1, end + 1))
+			pairs.remove((start, end))
+			escaped_nodes.append((start - 1, start, None))
+			escaped_nodes.append((end, end + 1, None))
+
+		if not len(pairs):
+			return sorted(escaped_nodes)
+
+		def build_interval_tree(remaining, lim=None):
+			nodes = []
+			while remaining and (lim is None or remaining[-1][0] < lim):
+				start, end = remaining.pop()
+				nodes.append((start, end, build_interval_tree(remaining, end)))
+			return nodes
+
+		tree = build_interval_tree(sorted(pairs, reverse=True))
+		return sorted(escaped_nodes + tree)
+
+	_spec_pattern = r'[<>=^]?[+\- ]?\d*(?:\.\d+)?[bcdeEfFgGnosxX%]?'
+	def parse_field(self, field):
+		content = field
+		terms = field.split(':')
+		spec = ''
+		if len(terms) > 1:
+			spec = terms[-1]
+			match = re.match(self._spec_pattern, spec)
+			if match is not None and match.group() == spec:
+				content = ':'.join(terms[:-1])
+			else:
+				spec = ''
+		conv = None
+		if content.endswith('!r') or content.endswith('!s') or content.endswith('!a'):
+			conv = content[-1]
+			content = content[:-2]
+		# ic(content, spec, conv)
+		return content, spec, conv
+
+	# def format(self, s, *args, **kwargs):
+	# 	return self.vformat(s, args, kwargs)
+	# def format_field(self, value, format_spec):
+	# 	return super().format_field(value, format_spec)
+	# def convert_field(self, value, conversion):
+	# 	return super().convert_field(value, conversion)
+
+	def variable_scope(self, expr, args, kwargs):
+		vars = self._expression_variables(expr)
+		scope = {}
+		for key in vars:
+			for src in args:
+				if key in src:
+					scope[key] = src[key]
+			else:
+				scope[key] = kwargs[key]
+		return scope
+
 	def get_field(self, field_name, args, kwargs):
 		try:
-			return super().get_field(field_name, args, kwargs)
-		except: # TODO: find the right exception
-			return eval(self.vformat(field_name, args, kwargs), kwargs), field_name
-			# return f'{{{field_name}}}', field_name
+			scope = self.variable_scope(field_name, args, kwargs)
+			out = eval(field_name, scope)
+		except SyntaxError:
+			out = self.vformat(field_name, args, kwargs)
+		return out, field_name
 
+	def _expression_variables(self, expr):
+		tree = ast.parse(expr, mode='eval')
 
-	def parse(self, s):
-		start_idx = -1
-		escaped = ''
-		pre_idx = 0
-		counter = 0
-		idx = 0
+		class VariableVisitor(ast.NodeVisitor):
+			def __init__(self):
+				self.variables = []
 
-		while idx < len(s):
-			open_idx = s.find("{", idx)
-			close_idx = s.find("}", idx)
+			def visit_Name(self, node):
+				if node.id not in self.variables:
+					self.variables.append(node.id)
 
-			if open_idx == -1 and close_idx == -1:
-				if counter == 0:
-					# raise StopIteration
-					# print(f'ending with: {escaped + s[idx:]!r}')
-					yield escaped + s[idx:], None, '', None
-				else:
-					raise ValueError("Mismatched '{' at index {}".format(start_idx))
-				break
+		visitor = VariableVisitor()
+		visitor.visit(tree)
+		return list(visitor.variables)
 
-			if open_idx != -1 and (open_idx < close_idx or close_idx == -1):
-				if counter == 0:
-					# yield (s[idx:open_idx], None)
-					start_idx = open_idx
-					pre_idx = idx
-				idx = open_idx + 1
-				counter += 1
-
-			if close_idx != -1 and (close_idx < open_idx or open_idx == -1):
-				if counter == 0:
-					raise ValueError("Mismatched '}' at index {}".format(close_idx))
-				counter -= 1
-				if counter == 0:
-					pre = s[pre_idx:start_idx]
-					field = s[start_idx + 1:close_idx]
-					if field.startswith("{") and field.endswith("}"):
-						escaped = pre + '{'
-
-						for lit, field, spec, conv in self.parse(field[1:-1]):
-							if escaped is not None:
-								lit = escaped + lit
-								escaped = None
-							yield lit, field, spec, conv
-
-						escaped = '}'
-
-					else:
-						# spec = None
-						lim = field.rfind('}')
-						conv_idx = field[lim+1:].find('!')
-						if conv_idx != -1:
-							conv = field[lim+2+conv_idx:]
-							field = field[:lim+1+conv_idx]
-						else:
-							conv = None
-
-						if conv is None:
-							lim = field.rfind(']')
-							spec_idx = field[lim+1:].find(':')
-							if spec_idx != -1:
-								spec = field[lim+2+spec_idx:]
-								field = field[:lim+1+spec_idx]
-							else:
-								spec = ''
-						else:
-							spec_idx = conv.find(':')
-							if spec_idx != -1:
-								spec = conv[spec_idx+1:]
-								conv = conv[:spec_idx]
-							else:
-								spec = ''
-
-						# print(f'yielding: {escaped + pre!r}, {field!r}, {spec!r}, {conv!r}')
-						# field = eval(self._format(field), self._world)
-						yield escaped + pre, field, spec, conv
-						escaped = ''
-					start_idx = -1
-				idx = close_idx + 1
+	def variables(self, s):
+		nodes = self.parse_bracket_tree(s)
+		past = set()
+		for start, end, children in nodes:
+			if children is None:
+				continue
+			elif len(children):
+				for var in self.variables(s[start + 1:end]):
+					if var not in past:
+						past.add(var)
+						yield var
+			else:
+				content, spec, conv = self.parse_field(s[start + 1:end])
+				try:
+					vars = self._expression_variables(content)
+				except SyntaxError:
+					continue
+				for var in vars:
+					if var not in past:
+						past.add(var)
+						yield var
 
 
 
-def pformat(s, **vars):
+# test_cases = {
+# 	'var!r': ('var', '', 'r'),
+# 	'var2:5.2g': ('var2', '5.2g', None),
+# 	'{1:0}!r': ('{1:0}', '', 'r'),
+# 	'{1:0.5}[1]:.2f': ('{1:0.5}[1]', '.2f', None),
+# 	'num:>10': ('num', '>10', None),
+# 	'num!r:.2f': ('num', '.2f', 'r'),
+# 	'{something_nice:.0f}_{num:>10}': ('{something_nice:.0f}_{num:>10}', '', None),
+# 	'{something_nice:.0f}_{num:>10}:>10': ('{something_nice:.0f}_{num:>10}', '>10', None),
+# }
+
+
+
+def pformat(s: str, /, *srcs: dict[str, Any], **manual: Any):
 	"""
 	Evaluates the keys in the given string as expressions using the given variables (recursively)
+
+	The inside of brackets is evaluated as an expression using the variables in srcs and manual.
+	Note that each of the `srcs` should behave like a dictionary implementing __getitem__ and __contains__.
+	If a necessary variable is not found in any of the `srcs`, it will be looked up in `manual`.
+	Also, all `srcs` and `manual` are checked each time a variable is looked up, so they can be updated.
+
+	Also, for convenience, you can also treat the content inside the brackets as a format string. This just means,
+	if you want something like this: `'{f"{x} and then {y}"}'`, you can just write `'{{x} and then {y}}'` instead.
+	Note however, that this will only work if the content inside the brackets is not a valid expression.
 	"""
 	fmt = PowerFormatter()
-	return fmt.format(s, **vars)
+	return fmt.vformat(s, srcs, manual)
 
+
+
+def pformat_vars(s: str):
+	"""
+	Returns the variables in the given string as expressions (recursively)
+
+	See `pformat` doc for more details.
+	"""
+	fmt = PowerFormatter()
+	yield from fmt.variables(s)
 
 
 
